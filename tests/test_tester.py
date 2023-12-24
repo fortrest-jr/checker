981164c55e8bc779e446b301cc63ba675e29ea9f
fortrest <ars.sobolev@gmail.com>
2023-12-24 22:53:01 +0600

    chore: add todos

--- tests/test_tester.py
+++ tests/test_tester.py
@@ -6,4 +6,9 @@ from checker import tester
 
 
 class TestTester:
+    # TODO: mock of course
+
+    # TODO: valid/invalid dir
+    # TODO: get_context
+        # TODO: add var in env, mocked pipline checks availability of env
     pass
