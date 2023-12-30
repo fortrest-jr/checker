b8ccb7bd00c07b7962285dc9122a4f29dd728a54
fortrest <ars.sobolev@gmail.com>
2023-12-30 21:31:51 +0600

    test: add mocks for plugin and pipeline

--- tests/test_tester.py
+++ tests/test_tester.py
@@ -1,14 +1,54 @@
 from __future__ import annotations
 
+from typing import Type
+
 import pytest
 
-from checker import tester
+from checker.configs import PipelineStageConfig
+from checker.plugins import PluginABC, PluginOutput
+
+
+class _OutputEnvPlugin(PluginABC):
+    name = 'env'
+
+    class Args(PluginABC.Args):
+        env_var: str
+
+    def _run(self, args: Args, *, verbose: bool = False) -> PluginOutput:
+        return PluginOutput(output=f'Env var value: {args.env_var}')
+
+
+@pytest.fixture
+def get_env_plugin() -> dict[str, Type[PluginABC]]:
+    return {
+        "env": _OutputEnvPlugin,
+    }
+
+
+@pytest.fixture
+def get_env_pipeline() -> list[PipelineStageConfig]:
+    return [
+        PipelineStageConfig(
+            name="stage1 - output env var value",
+            run="env",
+            args={"env_var": "${{ ENV_VAR }}"},
+        )
+    ]
 
 
 class TestTester:
     # TODO: mock of course
 
+    # TODO: test init
+
     # TODO: valid/invalid dir
     # TODO: get_context
-        # TODO: add var in env, mocked pipline checks availability of env
+    # TODO: add var in env, mocked pipline checks availability of env
+
+    # TODO: [X] mock of plugin,
+    #  [X] mock of pipeline,
+    #  [] send env var,
+    #  print env var in output,
+    #  check output
+    # TODO: test_pipeline.py
     pass
